507ad89418809318938ea8952f941d35ff6d3cdf
axel delor <axeldelor@gmail.com>
2026-04-11 11:59:03 +0200

    ajout affichage/ajouter favoris et navbar de connection

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState, useEffect } from "react";
 import { Card, Container, Row, Col } from "react-bootstrap";
 import Map from "./components/Map"
 import Favorite from "./components/Favorite";
+import NavBar from "./components/Navbar";
 import "./styles/App.css"
 
 const App = () => {
 
   const [bars, setBars] = useState([])
+  const [token, setToken] = useState(null)
+
 
   useEffect(() => {
     fetch("http://localhost:9000/bars")
@@ -15,18 +18,20 @@ const App = () => {
   }, []);
 
   console.log(bars)
+  console.log(token)
 
   return (
     <Container>
+      <NavBar setToken={setToken}/>
       <Row className="g-2 page-content p-3">
         <Col sm={12} lg={{ span: 8, order: 2 }}>
           <Card>
-            <Map className='map-card' bars={bars} />
+            <Map className='map-card' bars={bars} token={token} />
           </Card>
         </Col>
         <Col sm={12} lg={{ span: 4, order: 1 }}>
           <Card>
-            <Favorite className='favorites-card' bars={bars} />
+            <Favorite className='favorites-card' token={token} />
           </Card>
         </Col>
       </Row>
