d50255d7f477dfce32fbb49f51cdf06ded4fa3b0
axel delor <axeldelor@gmail.com>
2026-04-28 10:26:26 +0200

    markers leaflet pour compilation Vite

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,6 +4,18 @@ import { useMap } from "react-leaflet";
 import { useEffect, useRef } from "react";
 import "../styles/Map.css";
 
+import L from 'leaflet'
+import icon from 'leaflet/dist/images/marker-icon.png'
+import iconShadow from 'leaflet/dist/images/marker-shadow.png'
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconAnchor: [12, 41]
+})
+
+L.Marker.prototype.options.icon = DefaultIcon
+
 const FlyToBar = ({ selectedBar, markerRefs}) => {
   const map = useMap();
 
