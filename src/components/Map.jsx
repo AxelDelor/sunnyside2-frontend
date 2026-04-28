eaed918c4bcaa4acd535e023a50fd60ddfdadddb
axel delor <axeldelor@gmail.com>
2026-04-28 11:58:50 +0200

    timeout pour l'openpopup

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,50 +1,60 @@
-import { MapContainer, TileLayer, Popup, Marker, useMapEvent, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Popup,
+  Marker,
+  useMapEvent,
+  useMapEvents,
+} from "react-leaflet";
 import { Button, Stack } from "react-bootstrap";
 import { useMap } from "react-leaflet";
 import { useEffect, useRef, useState } from "react";
 import "../styles/Map.css";
 
-import L from 'leaflet'
-import icon from 'leaflet/dist/images/marker-icon.png'
-import iconShadow from 'leaflet/dist/images/marker-shadow.png'
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 let DefaultIcon = L.icon({
   iconUrl: icon,
   shadowUrl: iconShadow,
-  iconAnchor: [12, 41]
-})
+  iconAnchor: [12, 41],
+});
 
-L.Marker.prototype.options.icon = DefaultIcon
+L.Marker.prototype.options.icon = DefaultIcon;
 
-const FlyToBar = ({ selectedBar, markerRefs}) => {
+const FlyToBar = ({ selectedBar, markerRefs }) => {
   const map = useMap();
 
   useEffect(() => {
-    console.log(selectedBar)
+    console.log(selectedBar);
     if (selectedBar) {
       map.flyTo([selectedBar.latitude, selectedBar.longitude], 17);
-      markerRefs.current[selectedBar.id]?.openPopup()
+
+      setTimeout(() => {
+        markerRefs.current[selectedBar.id]?.openPopup();
+      }, 100);
     }
   }, [selectedBar]);
 
   return null;
 };
 
-const MapWatcher = ({onZoomChange, onBoundsChange}) => {
+const MapWatcher = ({ onZoomChange, onBoundsChange }) => {
   const map = useMapEvents({
     moveend: () => {
-      onBoundsChange(map.getBounds())
+      onBoundsChange(map.getBounds());
     },
     zoomend: () => {
-      onZoomChange(map.getZoom())
-    }
-  })
-  return null
-}
+      onZoomChange(map.getZoom());
+    },
+  });
+  return null;
+};
 
 const Map = ({ bars, token, setRefreshTrigger, selectedBar, favorites }) => {
-  const [currentZoom, setCurrentZoom] = useState(13)
-  const [bounds, setBounds] = useState(null)
+  const [currentZoom, setCurrentZoom] = useState(13);
+  const [bounds, setBounds] = useState(null);
 
   const handleFavorite = (id) => {
     const requestOptions = {
@@ -60,13 +70,15 @@ const Map = ({ bars, token, setRefreshTrigger, selectedBar, favorites }) => {
       .then(() => setRefreshTrigger((prev) => prev + 1));
   };
 
-  const markerRefs = useRef({})
+  const markerRefs = useRef({});
 
   const visibleBars = bounds
-        ? bars.filter( bar =>
+    ? bars.filter(
+        (bar) =>
           bounds.contains([bar.latitude, bar.longitude]) ||
           bar.id === selectedBar?.id
-        ) : bars
+      )
+    : bars;
 
   return (
     <>
@@ -75,31 +87,37 @@ const Map = ({ bars, token, setRefreshTrigger, selectedBar, favorites }) => {
         zoom={13}
         scrollWheelZoom={false}
       >
-        <MapWatcher onZoomChange={setCurrentZoom} onBoundsChange={setBounds}/>
+        <MapWatcher onZoomChange={setCurrentZoom} onBoundsChange={setBounds} />
         <FlyToBar selectedBar={selectedBar} markerRefs={markerRefs} />
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {visibleBars.map(({ id, name, latitude, longitude }) => {
-          const isFavorite = favorites.find(fav => fav.bar.id === id)
+          const isFavorite = favorites.find((fav) => fav.bar.id === id);
           return currentZoom >= 15 ? (
-          <Marker key={id} position={[latitude, longitude]} ref={el => markerRefs.current[id] = el}>
-            <Popup>
-              <Stack direction="vertical" className="text-center" gap={2}>
-                <span>{name}</span>
-                {token && token !== "null" ? (
-                  <Button
-                  value={id} onClick={() => handleFavorite(id)}
-                  disabled={!!isFavorite}
-                  variant={isFavorite ? "warning" : "primary"}>
-                    { isFavorite ? "Déjà ajouté" : "Ajouter" }
-                  </Button>
-                ) : null}
-              </Stack>
-            </Popup>
-          </Marker>
-          ) : null
+            <Marker
+              key={id}
+              position={[latitude, longitude]}
+              ref={(el) => (markerRefs.current[id] = el)}
+            >
+              <Popup>
+                <Stack direction="vertical" className="text-center" gap={2}>
+                  <span>{name}</span>
+                  {token && token !== "null" ? (
+                    <Button
+                      value={id}
+                      onClick={() => handleFavorite(id)}
+                      disabled={!!isFavorite}
+                      variant={isFavorite ? "warning" : "primary"}
+                    >
+                      {isFavorite ? "Déjà ajouté" : "Ajouter"}
+                    </Button>
+                  ) : null}
+                </Stack>
+              </Popup>
+            </Marker>
+          ) : null;
         })}
       </MapContainer>
     </>
